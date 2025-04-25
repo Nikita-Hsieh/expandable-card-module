2f14ccffd77b4a1905fcf6e5d9a788471237d224
Nikitahh <nikitahsieh03@gmail.com>
2025-04-25 19:28:51 +0800

    content with switch interactive animation

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.item').forEach((item) => {
+	item.addEventListener('click', () => {
+		document
+			.querySelectorAll('.item')
+			.forEach((i) => i.classList.remove('active'))
+
+		item.classList.add('active')
+	})
+})
